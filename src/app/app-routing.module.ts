7977431d135c9e654ba9f2ea797c3bfcc34f2e84
Mark Csoma <84617390+midorisol@users.noreply.github.com>
2022-11-08 14:42:54 +0100

    Finished hw

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo: '/posts',
+    pathMatch: 'full'
+  },
+  {
+    path: 'posts',
+    loadChildren: () =>
+      import('./posts/posts.module').then(m => m.PostsModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/posts/models/post-list-item.model.ts
@@ -0,0 +1,12 @@
+export class PostListItem {
+  id: number;
+  title: string;
+
+  constructor(
+    id: number,
+    title: string,
+  ) {
+    this.id = id
+    this.title  = title
+  }
+}
